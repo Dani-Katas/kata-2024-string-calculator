ca381cc842630aa6c870f94bca7ae140cac6f753
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-02-16 17:25:57 +0000

    refactor: extract class

--- src/StringCalculator.spec.ts
+++ src/StringCalculator.spec.ts
@@ -1,12 +1,5 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
-
-class StringCalculator {
-
-  add(numbers: string) {
-    return 0
-  }
-}
+import { describe, expect, it } from "vitest";
+import { StringCalculator } from "./StringCalculator.js";
 
 describe("StringCalculator", () => {
   it("returns zero with empty string", () => {
---
+++ src/StringCalculator.ts
@@ -0,0 +1,6 @@
+export class StringCalculator {
+
+  add(numbers: string) {
+    return 0;
+  }
+}
