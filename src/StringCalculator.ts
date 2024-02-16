2a03217ffa6a81968b212d1fa201cbd4afed0518
Daniel Ramos <danielramosacosta1@gmail.com>
2024-02-16 18:44:48 +0100

    red: returns the sum of three numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,6 +2,13 @@ export class StringCalculator {
 
   add(numbers: string) {
     let nums = numbers.split(",");
+
+    let sum = 0;
+    for (const number of nums) {
+      sum += Number(number);
+    }
+    return sum;
+
     if (nums.length > 1) {
       return Number(nums[0]) + Number(nums[1]);
     }
