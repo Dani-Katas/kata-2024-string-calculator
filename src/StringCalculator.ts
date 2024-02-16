37b9916db7a4e8d1b397cb9e3598763f5ce9ac20
Daniel Ramos <danielramosacosta1@gmail.com>
2024-02-16 18:50:05 +0100

    refactor: extract method parse

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,9 +1,13 @@
 export class StringCalculator {
 
   add(inputText: string) {
+    return this.parse(inputText)
+      .reduce((acc, curr) => acc + curr, 0);
+  }
+
+  private parse(inputText: string) {
     return inputText
       .split(",")
-      .map((num) => Number(num))
-      .reduce((acc, curr) => acc + curr, 0);
+      .map((num) => Number(num));
   }
 }
