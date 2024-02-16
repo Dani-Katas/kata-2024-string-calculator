e9f819f19321642d3364f2dbf1cbba19eeaac91e
Eduardo Pérez Suárez <e.ogex.be5.dev.cdo@telefonica.com>
2024-02-16 17:36:13 +0000

    green: returns the sum of two numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,6 +1,10 @@
 export class StringCalculator {
 
   add(numbers: string) {
+    let nums = numbers.split(",");
+    if (nums.length > 1) {
+      return Number(nums[0]) + Number(nums[1]);
+    }
     return Number(numbers);
   }
 }
