4259dbcf1cff026c851fbc902ad4ef16290f44d6
Daniel Ramos <danielramosacosta1@gmail.com>
2024-02-16 18:53:48 +0100

    refactor: extract StringCalculator instantiation to beforeEach

--- src/StringCalculator.spec.ts
+++ src/StringCalculator.spec.ts
@@ -1,42 +1,45 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { StringCalculator } from "./StringCalculator.js";
 
 describe("StringCalculator", () => {
+
+  let stringCalculator: StringCalculator;
+
+  beforeEach(() => {
+    stringCalculator = new StringCalculator();
+  });
+
   describe("Iteration 1", () => {
     it("returns zero with empty string", () => {
       const numbers = "";
 
-      let calculator = new StringCalculator();
-      const result = calculator.add(numbers);
+      const result = stringCalculator.add(numbers);
 
-      expect(result).toBe(0)
-    })
+      expect(result).toBe(0);
+    });
     it("returns the number with one number", () => {
-      const numbers = "1"
+      const numbers = "1";
 
-      let calculator = new StringCalculator();
-      const result = calculator.add(numbers);
+      const result = stringCalculator.add(numbers);
 
-      expect(result).toBe(1)
-    })
+      expect(result).toBe(1);
+    });
     it("returns the sum of two numbers", () => {
-      const numbers = "1,2"
+      const numbers = "1,2";
 
-      let calculator = new StringCalculator();
-      const result = calculator.add(numbers);
+      const result = stringCalculator.add(numbers);
 
-      expect(result).toBe(3)
-    })
-  })
+      expect(result).toBe(3);
+    });
+  });
 
   describe("Iteration 2", () => {
     it("returns the sum of three numbers", () => {
-      const numbers = "1,2,3"
+      const numbers = "1,2,3";
 
-      let calculator = new StringCalculator();
-      const result = calculator.add(numbers);
+      const result = stringCalculator.add(numbers);
 
-      expect(result).toBe(6)
-    })
-  })
-})
+      expect(result).toBe(6);
+    });
+  });
+});
