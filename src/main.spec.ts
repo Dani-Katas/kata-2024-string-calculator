906283705c830576654b3cc22e0807c289cf48ff
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-02-16 17:24:24 +0000

    green: returns zero with empty string

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,18 @@
 import { describe, it, expect } from "vitest"
 import { myFunction } from "./main.js"
 
+class StringCalculator {
+
+  add(numbers: string) {
+    return 0
+  }
+}
+
 describe("StringCalculator", () => {
   it("returns zero with empty string", () => {
     const numbers = "";
 
+    let calculator = new StringCalculator();
     const result = calculator.add(numbers);
 
     expect(result).toBe(0)
