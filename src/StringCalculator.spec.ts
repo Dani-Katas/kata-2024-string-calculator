ba079cd4afc6555a3893cb80a3c12a70f210b72f
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-02-16 18:07:46 +0000

    refactor: style with prettier

--- src/StringCalculator.spec.ts
+++ src/StringCalculator.spec.ts
@@ -1,55 +1,54 @@
-import { beforeEach, describe, expect, it } from "vitest";
-import { StringCalculator } from "./StringCalculator.js";
+import { beforeEach, describe, expect, it } from "vitest"
+import { StringCalculator } from "./StringCalculator.js"
 
 describe("StringCalculator", () => {
-
-  let stringCalculator: StringCalculator;
+  let stringCalculator: StringCalculator
 
   beforeEach(() => {
-    stringCalculator = new StringCalculator();
-  });
+    stringCalculator = new StringCalculator()
+  })
 
   describe("Iteration 1", () => {
     it("returns zero with empty string", () => {
-      const numbers = "";
+      const numbers = ""
 
-      const result = stringCalculator.add(numbers);
+      const result = stringCalculator.add(numbers)
 
-      expect(result).toBe(0);
-    });
+      expect(result).toBe(0)
+    })
     it("returns the number with one number", () => {
-      const numbers = "1";
+      const numbers = "1"
 
-      const result = stringCalculator.add(numbers);
+      const result = stringCalculator.add(numbers)
 
-      expect(result).toBe(1);
-    });
+      expect(result).toBe(1)
+    })
     it("sums two numbers", () => {
-      const numbers = "1,2";
+      const numbers = "1,2"
 
-      const result = stringCalculator.add(numbers);
+      const result = stringCalculator.add(numbers)
 
-      expect(result).toBe(3);
-    });
-  });
+      expect(result).toBe(3)
+    })
+  })
 
   describe("Iteration 2", () => {
     it("sums three numbers", () => {
-      const numbers = "1,2,3";
+      const numbers = "1,2,3"
 
-      const result = stringCalculator.add(numbers);
+      const result = stringCalculator.add(numbers)
 
-      expect(result).toBe(6);
-    });
-  });
+      expect(result).toBe(6)
+    })
+  })
 
   describe("Iteration 3", () => {
     it("sums numbers with newline as separator", () => {
-      const numbers = "1\n2,3";
+      const numbers = "1\n2,3"
 
-      const result = stringCalculator.add(numbers);
+      const result = stringCalculator.add(numbers)
 
-      expect(result).toBe(6);
-    });
-  });
-});
+      expect(result).toBe(6)
+    })
+  })
+})
