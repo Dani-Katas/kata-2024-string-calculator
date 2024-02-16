3965904ee2568589b0a64e96d2a9178b5a0802e5
Daniel Ramos <danielramosacosta1@gmail.com>
2024-02-16 19:32:16 +0100

    refactor: reformat file

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,22 +1,21 @@
 export class StringCalculator {
-
   add(inputText: string) {
-    const numbers = this.parse(inputText);
+    const numbers = this.parse(inputText)
 
-    return this.sumAll(numbers);
+    return this.sumAll(numbers)
   }
 
   private parse(inputText: string) {
     if (inputText.startsWith("//")) {
-      return [1,2]
+      return [1, 2]
     }
     return inputText
       .replace("\n", ",")
       .split(",")
-      .map((num) => Number(num));
+      .map((num) => Number(num))
   }
 
   private sumAll(numbers: number[]) {
-    return numbers.reduce((acc, curr) => acc + curr, 0);
+    return numbers.reduce((acc, curr) => acc + curr, 0)
   }
 }
