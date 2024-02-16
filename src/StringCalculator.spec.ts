361eb85ad072c7e2a26d440c0f5a762679258895
Eduardo Pérez Suárez <e.ogex.be5.dev.cdo@telefonica.com>
2024-02-16 17:39:32 +0000

    refactor:Nest tests

--- src/StringCalculator.spec.ts
+++ src/StringCalculator.spec.ts
@@ -2,28 +2,31 @@ import { describe, expect, it } from "vitest";
 import { StringCalculator } from "./StringCalculator.js";
 
 describe("StringCalculator", () => {
-  it("returns zero with empty string", () => {
-    const numbers = "";
+  describe("Iteration 1", () => {
+    it("returns zero with empty string", () => {
+      const numbers = "";
 
-    let calculator = new StringCalculator();
-    const result = calculator.add(numbers);
+      let calculator = new StringCalculator();
+      const result = calculator.add(numbers);
 
-    expect(result).toBe(0)
+      expect(result).toBe(0)
+    })
+    it("returns the number with one number", () => {
+      const numbers = "1"
+
+      let calculator = new StringCalculator();
+      const result = calculator.add(numbers);
+
+      expect(result).toBe(1)
+    })
+    it("returns the sum of two numbers", () => {
+      const numbers = "1,2"
+
+      let calculator = new StringCalculator();
+      const result = calculator.add(numbers);
+
+      expect(result).toBe(3)
+    })
   })
-  it("returns the number with one number", () => {
-    const numbers = "1"
 
-    let calculator = new StringCalculator();
-    const result = calculator.add(numbers);
-
-    expect(result).toBe(1)
-  })
-  it("returns the sum of two numbers", () => {
-    const numbers = "1,2"
-
-    let calculator = new StringCalculator();
-    const result = calculator.add(numbers);
-
-    expect(result).toBe(3)
-  })
 })
